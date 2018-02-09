992c5bd78538ddb15c21395fb41cb08da8f30fa2
Korobeynik <korobeynik77@gmail.com>
2018-02-09 17:25:00 +0200

    Prop types

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Создание елемента React
-//React.createElement("Название DOM елемента", "Свойство", "Содержимое елемента")
- const title = React.createElement('h1', null , 'React Todo' );
- const subtitle = React.createElement('p', { className: 'subtitle' }, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas sint nobis soluta cupiditate corrupti et, nisi atque dolorem, laborum minima sunt delectus qui voluptates perspiciatis. Porro eveniet a magnam odio.');
-const container = React.createElement('div', null, title, subtitle);
-const app = (
-		<div>
-			<h1>React Todo</h1>
-			<p className="subtitle">This is my first app on React</p>
-		</div>
-);
-
+//Компонент
+function App(props) {
+	return (
+		<main>
+			<header>
+				<h1>{props.title}</h1>
+			</header>
+			<section className="todo-list">
+				<div className="todo comleted">
+					<button className="checkbox icon">
+						<i className="material-icons">check_box</i>
+					</button>
+					<span className="todo-title">Изучить Angular</span>
+					<button className="delete icon">
+						<i className="material-icons">delete</i>
+					</button>
+				</div>
+				<div className="todo">
+					<button className="checkbox icon">
+						<i className="material-icons">check_box_outline_blank</i>
+					</button>
+					<span className="todo-title">Написать приложение</span>
+					<button className="delete icon">
+						<i className="material-icons">delete</i>
+					</button>
+				</div>
+				<div className="todo"> 
+					<button className="checkbox icon">
+						<i className="material-icons">check_box_outline_blank</i>
+					</button>
+					<span className="todo-title">Изучить React</span>
+					<button className="delete icon">
+						<i className="material-icons">delete</i>
+					</button>
+				</div>
+			</section>
+		</main>
+	);
+}
+ 
  //Вывод обьекта на страницу методом Render
- ReactDOM.render(app, document.getElementById('root'));
+ ReactDOM.render(<App title="React Todo" />, document.getElementById('root'));
